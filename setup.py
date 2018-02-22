3bd1da5de366d85f74035316cd31d5651231c59b
Johann Bauer <bauerj@bauerj.eu>
2018-02-22 21:25:16 +0100

    Build Windows wheels on appveyor

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 groestlcoin_hash_module = Extension('groestlcoin_hash', sources = ['groestlcoinmodule.c', 'groestl.c'])
 
