4f94af9b15e562c371353c98e074624d7c0a3ee9
gruve-p <gruve-p@users.noreply.github.com>
2020-04-14 23:27:19 +0200

    Bump to 1.0.2

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, Extension
 groestlcoin_hash_module = Extension('groestlcoin_hash', sources = ['groestlcoinmodule.c', 'groestl.c'])
 
 setup (name = 'groestlcoin_hash',
-       version = '1.0.1',
+       version = '1.0.2',
        description = 'Groestlcoin hash algorithm.',
-       maintainer = 'Kefkius',
-       maintainer_email = 'kefkius@mail.com',
+       maintainer = 'Groestlcoin Developers',
+       maintainer_email = 'groestlcoin@gmail.com',
        url = 'https://github.com/Groestlcoin/groestlcoin-hash-python',
        keywords = ['groestlcoin', 'grs'],
        ext_modules = [groestlcoin_hash_module])
